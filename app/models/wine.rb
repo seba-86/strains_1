c205b46286abdfb3dff63cb12d73b7c3ddb6879d
Sebastian Venegas <sebastian@verdefresco.cl>
2021-03-17 13:18:35 -0300

    simple_form_checkbox

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,4 +1,6 @@
 class Wine < ApplicationRecord
     has_many :assemblies
     has_many :strains, through: :assemblies, dependent: :destroy
+
+    validates_presence_of :name, on: :create, message: "can't be blank"
 end
